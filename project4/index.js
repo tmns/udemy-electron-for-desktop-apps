16aad4401b3a912ee117aee31be62389b29a7f31
sairim <spiri@protonmail.com>
2019-04-12 14:17:48 +0300

    add complete project4 which explored the combination of react, redux, and electron through a video converter app

---
+++ project4/index.js
@@ -0,0 +1,54 @@
+const electron = require('electron');
+const ffmeg = require('fluent-ffmpeg');
+const _ = require('lodash');
+
+const { app, BrowserWindow, ipcMain, shell } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreferences: { backgroundThrottling: false }
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+})
+
+ipcMain.on('videos:added', (event, videos) => {
+    const promises = _.map(videos, video => {
+        return new Promise((resolve, reject) => {
+            ffmpeg.ffprobe(video.path, (error, metadata) => {
+                video.duration = metadata.format.duration;
+                video.format = 'avi';
+                resolve(video);
+            });
+        });
+    });
+
+    Promise.all(promises)
+        .then((results) => {
+            mainWindow.webContents.send('metadata:complete', results)
+        })
+});
+
+ipcMain.on('conversion:start', (event, videos) => {
+    _.each(videos, video => {
+        const outputDirectory = video.path.split(video.name)[0];
+        const outputName = video.name.split('.')[0];
+        const outputPath = `${outputDirectory}${outputName}.${video.format}`
+
+        ffmeg(video.path)
+            .output(outputPath)
+            .on('progress', ({ timemark }) => {
+                mainWindow.webContents.send('conversion:progress', { video, timemark })
+            })
+            .on('end', () => mainWindow.webContents.send('conversion:end', { video, outputPath }))
+            .run();
+    })
+})
+
+ipcMain.on('folder:open', (event, outputPath) => {
+    shell.showItemInFolder(outputPath);
+})
